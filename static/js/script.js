efacd51c12475556bb3fba53bff69a8f92e83835
Ananth Kini <ananthkini1@gmail.com>
2026-01-11 11:53:58 -0800

    feat: added disease name mapping and formatting

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,55 @@ document.getElementById("imageInput").addEventListener("change", (e) => {
     document.getElementById("fileName").textContent = fileName;
 });
 
+/// Disease name mapping
+const diseaseNames = {
+    'DR': 'Diabetic Retinopathy',
+    'ARMD': 'Age-Related Macular Degeneration',
+    'MH': 'Macular Hole',
+    'DN': 'Drusen',
+    'MYA': 'Myopia',
+    'BRVO': 'Branch Retinal Vein Occlusion',
+    'TSLN': 'Tessellation',
+    'ERM': 'Epiretinal Membrane',
+    'LS': 'Laser Scars',
+    'MS': 'Macular Scar',
+    'CSR': 'Central Serous Retinopathy',
+    'ODC': 'Optic Disc Cupping',
+    'CRVO': 'Central Retinal Vein Occlusion',
+    'TV': 'Tortuous Vessels',
+    'AH': 'Asteroid Hyalosis',
+    'ODP': 'Optic Disc Pallor',
+    'ODE': 'Optic Disc Edema',
+    'ST': 'Optociliary Shunt',
+    'AION': 'Anterior Ischemic Optic Neuropathy',
+    'PT': 'Parafoveal Telangiectasia',
+    'RT': 'Retinal Traction',
+    'RS': 'Retinitis',
+    'CRS': 'Chorioretinitis',
+    'EDN': 'Exudation',
+    'RPEC': 'Retinal Pigment Epithelium Changes',
+    'MHL': 'Macular Hole (Lamellar)',
+    'RP': 'Retinitis Pigmentosa',
+    'CWS': 'Cotton Wool Spots',
+    'CB': 'Coloboma',
+    'ODPM': 'Optic Disc Pit Maculopathy',
+    'PRH': 'Preretinal Hemorrhage',
+    'MNF': 'Myelinated Nerve Fibers',
+    'HR': 'Hemorrhagic Retinopathy',
+    'CRAO': 'Central Retinal Artery Occlusion',
+    'TD': "Tilted Disc",
+    'CME': 'Cystoid Macular Edema',
+    'PTCR': 'Post-Traumatic Choroidal Rupture',
+    'CF': 'Choroidal Folds',
+    'VH': 'Vitreous Hemorrhage',
+    'MCA': 'Macroaneurysm',
+    'VS': 'Vasculitis',
+    'BRAO': 'Branch Retinal Artery Occlusion',
+    'PLQ': 'Plaque',
+    'HPED': 'Hemorrhagic Pigment Epithelial Detachment',
+    'CL': 'Chorioretinal Lesion'
+};
+
 // Main function to upload and analyze retinal images
 function uploadImage() {
     const fileInput = document.getElementById('imageInput');
@@ -53,8 +102,10 @@ function displayResults(file, predictions) {
     predictions.forEach((pred, index) => {
         const predItem = document.createElement('div');
         predItem.className = 'prediction-item';
+        const fullName = diseaseNames[pred.disease] || pred.disease;
+        const displayName = diseaseNames[pred.disease] ? `${fullName} (${pred.disease})` : pred.disease;
         predItem.innerHTML = `
-            <span class="disease-name">${index + 1}. ${pred.disease}</span>
+            <span class="disease-name">${index + 1}. ${displayName}</span>
             <span class="probability">${pred.percentage}</span>
         `;
         predictionsDiv.appendChild(predItem);
